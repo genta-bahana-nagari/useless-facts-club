64e29d2c8e8ac000ba4add8d0dab3c21fe890fb9
Genta <gentapossenti@gmail.com>
2025-06-18 23:34:51 +0700

    basic frontend

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import { trpc, trpcClientOptions } from '@/utils/trpc';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+const queryClient = new QueryClient();
+
+export default function App({ Component, pageProps }: any) {
+  return (
+    <trpc.Provider client={trpc.createClient(trpcClientOptions)} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </trpc.Provider>
+  );
 }
