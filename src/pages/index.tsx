962719d7e76839165702c539feb9c6fd5d0a6332
Genta <gentapossenti@gmail.com>
2025-06-20 17:49:41 +0700

    plis deh

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay, Pagination } from 'swiper/modules';
+import { Autoplay, } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import { trpc } from "@/utils/trpc";
